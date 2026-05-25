d7a739c7ac2bc2ae3ae139dd6b2918bef816b40c
Manas Tripathi <100483566+godspeedx14@users.noreply.github.com>
2026-05-25 22:39:38 +0530

    May 25, 2026 at 10:39 PM dsa practice_automated sync

---
+++ Hfhbvv.cpp
@@ -0,0 +1,36 @@
+```cpp
+/*
+Core Idea: This function identifies the first index 'i' where the difference between the maximum value in the prefix subarray nums[0...i] and the minimum value in the suffix subarray nums[i...n-1] is less than or equal to 'k'. It precomputes prefix maximums and suffix minimums to efficiently check this condition for each 'i'.
+Time Complexity: O(n), due to three linear passes over the input array (one for prefix maximums, one for suffix minimums, and one for checking the condition).
+Space Complexity: O(n), for storing the prefix maximums and suffix minimums in two auxiliary vectors.
+*/
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        vector<int>mx,mn;
+
+        int n=nums.size();
+        int mxr=nums[0],mnr=nums[n-1];
+        for (int i =0;i<n;i++)
+            {
+                mxr=max(mxr,nums[i]);
+                mx.push_back(mxr);
+            }
+        for(int i=n-1;i>=0;i--)
+            {
+                mnr=min(mnr,nums[i]);
+                mn.push_back(mnr);
+            }
+
+        int res=INT_MAX;
+
+        for(int i=0;i<n;i++)
+            {
+                int x=mx[i]-mn[n-1-i];
+                if(x<=k)
+                    return i;
+            }
+        return -1;
+    }
+};
+```
